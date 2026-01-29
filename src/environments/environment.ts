39bc05f5c70636885b175352351ae36e886ed6d1
Manipamarthi10 <manikantap1010@gmail.com>
2026-01-29 10:22:37 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   tmdb: {
-    apiKey: 'e501d393039f2cf041371b56f69f6572',
+    apiKey: 'api',
     apiBaseUrl: 'https://api.themoviedb.org/3',
     imageBaseUrl: 'https://image.tmdb.org/t/p/w500'
   }
